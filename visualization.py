7850c2a96884c4e9c80f22398c430f683306ea66
hong-dev <jjuggumih@gmail.com>
2020-05-14 02:14:36 +0900

    create dataframe for table 1. add normalized data (between 0 and 100) 2. add rank for two columns(individuals online, bed-places) 3. add average rank according to two rank data(#2) 4. change missing data: from 'NaN' to '0' (data를 정규화 할 때(#1), missing data일 때만 가장 낮은 점수 0으로 반영하도록 변경)

--- visualization.py
+++ visualization.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 plt.style.use('seaborn')
 
 df = pd.read_csv(
@@ -34,8 +36,34 @@ bar.right_ax.set_ylabel(
 
 plt.savefig('bar_plot')
 
-
 # table
+original_table   = df
+normalized_table = original_table.copy()
 
+normalized_table[ : ] = MinMaxScaler().fit_transform(normalized_table[ : ]) * 100
+
+normalized_table = normalized_table.rename(
+    columns = {
+        'Percentage of individuals online': '(Nor) Individuals online',
+        'Number of Bed-places'            : '(Nor) Bed-places'
+    }
+)
+
+merged_table = pd.merge(original_table, normalized_table, on = 'Country Code')
+
+merged_table['(Rank) Individuals online'] = (
+    merged_table['Percentage of individuals online']
+    .rank(ascending = False, method = 'min')
+)
+merged_table['(Rank) Bed-places'] = (
+    merged_table['Number of Bed-places']
+    .rank(ascending = False, method = 'min')
+)
+merged_table['(Rank) Avg Rank'] = (
+    merged_table[['(Rank) Individuals online', '(Rank) Bed-places']]
+    .mean(axis = 1)
+)
+
+merged_table = merged_table.sort_values('(Rank) Avg Rank', ascending = True)
 
 # normalized_plot
